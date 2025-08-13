296f09a4c4a252009e14610e6d38a9c0e2ad5bbe
Zawaer <zawarudo123pro@gmail.com>
2025-08-13 16:19:21 +0300

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,45 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen bg-gray-950">
+			<div className="min-w-150 overflow-hidden rounded-md border border-gray-700">
+				<table className="w-full max-w-4xl text-left border-collapse">
+					<thead className="bg-gray-900 border-b border-gray-700">
+						<tr>
+							<th className="px-4 py-2 text-gray-200">Status</th>
+							<th className="px-4 py-2 text-gray-200">Room</th>
+							<th className="px-4 py-2 text-gray-200">Last seen</th>
+						</tr>
+					</thead>
+					<tbody className="bg-gray-800 divide-y divide-gray-700">
+						<tr>
+							<td className="px-4 py-2 text-emerald-400 flex items-center gap-2">
+								<span className="material-symbols-outlined">sensors</span>
+								Broadcasting
+							</td>
+							<td className="px-4 py-2 text-gray-500">1315</td>
+							<td className="px-4 py-2 text-gray-500">48m ago</td>
+						</tr>
+						<tr>
+							<td className="px-4 py-2 text-orange-400 flex items-center gap-2">
+								<span className="material-symbols-outlined">sensors_off</span>
+								Idle
+							</td>
+							<td className="px-4 py-2 text-gray-500">2408</td>
+							<td className="px-4 py-2 text-gray-500">31m ago</td>
+						</tr>
+						<tr>
+							<td className="px-4 py-2 text-red-400 flex items-center gap-2">
+								<span className="material-symbols-outlined">exclamation</span>
+								Offline
+							</td>
+							<td className="px-4 py-2 text-gray-500">3227</td>
+							<td className="px-4 py-2 text-gray-500">1h 38m ago</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	);
 }
